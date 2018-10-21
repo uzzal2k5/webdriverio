359cb166b2b4fb1f51d02cca853be55164a5a765
uzzal2k5 <uzzal2k5@gmail.com>
2018-10-22 03:33:19 +0600

    Update test

--- examples/test/lib/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-// Object.defineProperty(exports, '__esModule', { value: true })
-const Reporter1 = require('./commons/ReporterUtils')
-exports.Reporter = Reporter1.Reporter
-const TestUtils1 = require('./commons/TestUtils')
-exports.TestUtils = TestUtils1.TestUtils
